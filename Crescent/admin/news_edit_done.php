ff52b1e84d8a50e849a6c5bf53300fa266a43791
y.asano <tennis6293@gmail.com>
2023-10-12 10:55:09 +0900

    add:04 news_edit_done.php

---
+++ Crescent/admin/news_edit_done.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+session_start();
+require_once(dirname(__FILE__) . '/auth.inc.php');
+$loingVerfiy = authConfirm();
+
+require_once(dirname(__FILE__) . '/../util.inc.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>お知らせの編集完了 | Crescent Shoes 管理</title>
+    <link rel="stylesheet" href="css/admin.css">
+</head>
+
+<body>
+    <header>
+        <div class="inner">
+            <span><a href="index.php">Crescent Shoes 管理</a></span>
+            <?php include dirname(__FILE__) . '/account.parts.php'; ?>
+        </div>
+    </header>
+    <div class="container">
+        <main>
+            <h1>お知らせの編集</h1>
+            <p>お知らせの編集が完了しました。</p>
+            <p><a href="index.php">お知らせ一覧へ戻る</a></p>
+        </main>
+        <footer>
+            <p>&copy; Crescent Shoes All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+
+</html>
